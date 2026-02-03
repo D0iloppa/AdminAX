2ef74dbe2888a48494ab39e306ca03c4d6ff72de
D0iloppa <kdi3939@gmail.com>
2026-02-03 14:57:20 +0900

    init

--- engine/AdminAX-engine/src/main/java/com/adminax/engine/controller/TestController.java
+++ engine/AdminAX-engine/src/main/java/com/adminax/engine/controller/TestController.java
@@ -17,6 +17,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("/hello")
     public String hello() {
-        return "AdminAX Engine is Alive! Time: " + LocalDateTime.now();
+        return "🤠AdminAX Engine is Alive! Time: " + LocalDateTime.now();
     }
 }
