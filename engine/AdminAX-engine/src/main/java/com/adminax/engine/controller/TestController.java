51561a788ce2438f85d57aa74153040aed2cd343
D0iloppa <kdi3939@gmail.com>
2026-02-03 12:44:15 +0900

    test controller

---
+++ engine/AdminAX-engine/src/main/java/com/adminax/engine/controller/TestController.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.adminax.engine.controller;
+
+import java.time.LocalDateTime;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 
+ */
+@RestController
+@RequestMapping("/test")
+public class TestController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "AdminAX Engine is Alive! Time: " + LocalDateTime.now();
+    }
+}
