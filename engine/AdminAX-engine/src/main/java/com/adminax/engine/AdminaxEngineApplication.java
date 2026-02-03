c21dcc22fe43a1bc475e80a5b7118d71af48bf45
D0iloppa <kdi3939@gmail.com>
2026-02-03 11:08:31 +0900

    init

---
+++ engine/AdminAX-engine/src/main/java/com/adminax/engine/AdminaxEngineApplication.java
@@ -0,0 +1,13 @@
+package com.adminax.engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminaxEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminaxEngineApplication.class, args);
+	}
+
+}
---
+++ engine/AdminAX-engine/src/test/java/com/adminax/engine/AdminaxEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adminax.engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminaxEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
